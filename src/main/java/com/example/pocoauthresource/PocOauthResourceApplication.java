9576c0d9f3ca1e5a8221fe6b752847b6c9608a7e
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-02-08 17:53:48 -0200

    Resource server 1o end-point

--- src/main/java/com/example/pocoauthresource/PocOauthResourceApplication.java
+++ src/main/java/com/example/pocoauthresource/PocOauthResourceApplication.java
@@ -1,14 +1,61 @@
 package com.example.pocoauthresource;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PocOauthResourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocOauthResourceApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public Message home() {
+	  return new Message("Hello World FM RESOURCE SERVER x");
+	}
+
+	@RequestMapping("/resource")
+	public Map<String,Object> resource() {
+	  Map<String,Object> model = new HashMap<String,Object>();
+	  model.put("id", UUID.randomUUID().toString());
+	  model.put("content", "Hello World FM 2");
+	  return model;
+	}
+
 }
+
+class Message {
+	
+	private String id = UUID.randomUUID().toString();
+	private String content;
+	
+	public Message(String content) {
+	  this.content = content;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return this.content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+  }
 
