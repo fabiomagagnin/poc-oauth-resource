d9f36808f3f9c6a1c9461b7c9df93220bcc88d12
Fábio Magagnin <fabio.magagnin@meta.com.br>
2019-02-08 17:33:18 -0200

    first commit

---
+++ src/main/java/com/example/pocoauthresource/PocOauthResourceApplication.java
@@ -0,0 +1,14 @@
+package com.example.pocoauthresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocOauthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocOauthResourceApplication.class, args);
+	}
+
+}
+
